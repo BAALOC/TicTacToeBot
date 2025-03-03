617a14bd418d2f688a23c7fa1d346b1eb759bc5f
makar <makarleonardov@gmail.com>
2025-03-03 21:31:46 +0300

    added README.md

--- utils/misc/setup_logger.py
+++ utils/misc/setup_logger.py
@@ -1,15 +1,18 @@
 import logging
 
-# TODO: Добавить логирование в файл
 def bot_setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
-    handler = logging.StreamHandler()  
-    handler.setFormatter(formatter)
+    file_handler = logging.FileHandler('bot.log')
+    file_handler.setFormatter(formatter)
+    
+    stream_handler = logging.StreamHandler()  
+    stream_handler.setFormatter(formatter)
     
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logger.addHandler(handler)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
     
     return logger
 
