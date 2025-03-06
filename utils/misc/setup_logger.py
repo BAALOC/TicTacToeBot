4656845f13668f78763c5a4daab35ff57be31448
makar <makarleonardov@gmail.com>
2025-03-06 18:02:56 +0300

    fixed logger

--- utils/misc/setup_logger.py
+++ utils/misc/setup_logger.py
@@ -3,15 +3,13 @@ import logging
 def bot_setup_logger(name: str, level: int = logging.INFO) -> logging.Logger:
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
-    file_handler = logging.FileHandler('bot.log')
-    file_handler.setFormatter(formatter)
+
     
     stream_handler = logging.StreamHandler()  
     stream_handler.setFormatter(formatter)
     
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
     
     return logger
